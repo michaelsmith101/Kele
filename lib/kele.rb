8ca7dc47f535a3aeea58fb8eed7d774388f35323
Michael Smith <michaelsmith101@gmail.com>
2017-12-27 03:11:21 +0000

    checkpoint 4 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,4 +15,9 @@ require 'httparty'
        JSON.parse(response.body)
     end
     
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
+    
 end
